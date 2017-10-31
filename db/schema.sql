204cac49c48f8a138badaea1f07d8f1576b95500
kevdawg21 <kevinmyin@yahoo.com>
2017-10-30 23:08:40 -0700

    6

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,10 @@
 ### Schema
 
-CREATE DATABASE cat_db;
-USE cat_db;
-
-CREATE TABLE cats
-(
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
+create database burgers_db;
+use burgers_db;
+create table burgers (
+	id int(10) primary key not null auto_increment,
+	burger_name varchar(50),
+	devoured boolean,
+	date: date
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,3 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+insert into burgers (burger_name, devoured) values ("Caribbean", false);
+insert into burgers (burger_name, devoured) values ("Pesto", false);
+insert into burgers (burger_name, devoured) values ("Western", false);	
